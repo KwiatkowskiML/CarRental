016cb7cde9eff52f2d57296cfceba44cf0629b44
WojciechMat <wmatsad@gmail.com>
2024-10-16 18:16:11 +0200

    add contact to integrator

--- sql/tables/integrators.sql
+++ sql/tables/integrators.sql
@@ -2,5 +2,7 @@ CREATE TABLE integrators (
     integrator_id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     api_key VARCHAR(255) UNIQUE NOT NULL,
+    contact_email VARCHAR(255) NOT NULL,
+    contact_phone VARCHAR(50),
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
