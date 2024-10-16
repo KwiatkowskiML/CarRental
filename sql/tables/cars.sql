baf90415b96137b9b873987edbd41761d0057460
WojciechMat <wmatsad@gmail.com>
2024-10-16 17:52:50 +0200

    add licence_plate to cars table

--- sql/tables/cars.sql
+++ sql/tables/cars.sql
@@ -1,12 +1,13 @@
 CREATE TABLE cars (
     car_id SERIAL PRIMARY KEY,
+    license_plate VARCHAR(20) UNIQUE NOT NULL,
     brand VARCHAR(100) NOT NULL,
     model VARCHAR(100) NOT NULL,
     year INT NOT NULL,
     status VARCHAR(50) NOT NULL, -- Available, Rented, Maintenance, etc.
     location VARCHAR(255),
-    engine_capacity DECIMAL(3, 1) NOT NULL, -- in liters, e.g., 2.0, 3.5
-    power INT NOT NULL, -- in horsepower
-    fuel_type VARCHAR(50) NOT NULL, -- e.g., Petrol, Diesel, Electric, Hybrid
+    engine_capacity DECIMAL(3, 1) NOT NULL,
+    power INT NOT NULL,
+    fuel_type VARCHAR(50) NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
