4383b1dae8b6c9f86e60f9556a3b9464ac0ad940
kwiatkowskim <kwiatkowski.michal.2003@gmail.com>
2024-11-20 10:48:09 +0100

    rentals refactor

--- sql/tables/rentals.sql
+++ sql/tables/rentals.sql
@@ -1,12 +1,6 @@
 CREATE TABLE rentals (
     rental_id SERIAL PRIMARY KEY,
-    customer_id INT REFERENCES customers(customer_id),
-    insurance_id INT REFERENCES insurances(insurance_id),
-    car_id INT REFERENCES cars(car_id),
-    start_date DATE NOT NULL,
-    end_date DATE NOT NULL,
+    offer_id INT REFERENCES offers(offer_id),
     status VARCHAR(50) NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE,
-    has_gps BOOLEAN,
-    has_child_seat BOOLEAN
+    created_at TIMESTAMP WITH TIME ZONE
 );
