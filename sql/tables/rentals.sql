eb36d0ef01bb5214343528a78928518cb16097dc
WojciechMat <wmatsad@gmail.com>
2024-11-22 11:47:41 +0100

    unique constraint

--- sql/tables/rentals.sql
+++ sql/tables/rentals.sql
@@ -2,5 +2,6 @@ CREATE TABLE rentals (
     rental_id SERIAL PRIMARY KEY,
     offer_id INT REFERENCES offers(offer_id),
     status VARCHAR(50) NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE
+    created_at TIMESTAMP WITH TIME ZONE,
+    UNIQUE(offer_id)
 );
