7d5ca139073b80fb3a9ec55c3b4313f99ec50c60
WojciechMat <wmatsad@gmail.com>
2025-01-17 00:31:20 +0100

    control car image storage through environment variables

--- WebAPI/Constants/StorageConstants.cs
+++ WebAPI/Constants/StorageConstants.cs
@@ -2,6 +2,11 @@ namespace WebAPI.Constants;
 
 public static class StorageConstants
 {
-    public const string BucketName = "car-images-dev-0";
+    public static string BucketName { get; private set; } = "car-images-dev-0";
     public const string PublicUrlPrefix = "https://storage.googleapis.com";
+
+    public static void Initialize(string bucketName)
+    {
+        BucketName = bucketName;
+    }
 }
