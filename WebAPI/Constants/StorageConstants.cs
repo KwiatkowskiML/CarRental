152af44af747af0e1b8eb1822a29f9715510b548
kwiatkowskim <kwiatkowski.michal.2003@gmail.com>
2025-01-17 17:31:10 +0100

    quick fix

--- WebAPI/Constants/StorageConstants.cs
+++ WebAPI/Constants/StorageConstants.cs
@@ -3,11 +3,11 @@ namespace WebAPI.Constants;
 public static class StorageConstants
 {
     public static string BucketName { get; private set; } = "car-images-dev-0";
-    public const string PublicUrlPrefix = "https://storage.googleapis.com";
+    public static string PublicUrlPrefix = "https://storage.googleapis.com";
 
-    public static void Initialize(string bucketName, string PublicUrlPrefix)
+    public static void Initialize(string bucketName, string publicUrlPrefix)
     {
         BucketName = bucketName;
-        PublicUrlPrefix = PublicUrlPrefix;
+        PublicUrlPrefix = publicUrlPrefix;
     }
 }
